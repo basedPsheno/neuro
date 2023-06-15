06c8f19263464ed15a96c5c8e872b81fb1506a1a
basedPsheno <based.psheno@gmail.com>
2023-06-15 20:03:19 +0300

    2.1 #004

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ import tensorflow as tf
 
 def main():
     x = tf.range(12, dtype=tf.float32)
-    print(x.shape)
+    print(x)
+    X = tf.reshape(x, (3, 4))
+    print(X)
 
 
 if __name__ == "__main__":
