44b14b4b1ce44c88a63fe8eefc4eb2e134dc850b
basedPsheno <based.psheno@gmail.com>
2023-06-18 19:50:39 +0300

    2.1 #011

--- 2.1/main.py
+++ 2.1/main.py
@@ -2,9 +2,10 @@ import tensorflow as tf
 
 
 def main():
-    X = tf.random.normal((3, 4))
+    x = tf.range(12, dtype=tf.float32)
+    X = tf.reshape(x, (3, 4))
     X_var = tf.Variable(X)
-    X_var[1, 2].assign(9)
+    X_var[:2, :].assign(tf.ones(X_var[:2,:].shape, dtype=tf.float32) * 12)
     print(X_var)
 
     
