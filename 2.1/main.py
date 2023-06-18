9388162b35d9c052bfcd0fe7295d9c51f242d2b7
basedPsheno <based.psheno@gmail.com>
2023-06-18 19:29:24 +0300

    2.1 #009

--- 2.1/main.py
+++ 2.1/main.py
@@ -2,18 +2,9 @@ import tensorflow as tf
 
 
 def main():
-    x = tf.range(12, dtype=tf.float32)
-    print(x)
-    X = tf.reshape(x, (3, 4))
-    print(X)
-    Y = tf.zeros((2, 3, 4))
-    print(Y)
-    Z = tf.ones((2, 3, 4))
-    print(Z)
-    A = tf.random.normal(shape=[3, 4])
-    print(A)
-    B = tf.constant([[2, 1, 4, 3], [1, 2, 3, 4], [4, 3, 2, 1]])
-    print(B)
+    X = tf.random.normal((3, 4))
+    print(X[-1])
+    print(X[1:3])
 
     
 
